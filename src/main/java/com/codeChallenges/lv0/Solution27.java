f0213c7c3d94e131f39ac8031c6b640ec6554f1a
JJAJJAKIM <csm9121@gmail.com>
2025-12-16 20:10:28 +0900

    20251216 코드챌린지

---
+++ src/main/java/com/codeChallenges/lv0/Solution27.java
@@ -0,0 +1,117 @@
+package com.codeChallenges.lv0;
+
+import java.util.Scanner;
+
+public class Solution27 {
+    /* 1번 문제
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + usage * change[i] / 100;
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }*/
+    /* 2번 문제
+    public int solution(int seat, String[][] passengers) {
+        int num_passenger = 0;
+        for(int i=0; i<passengers.length; i++){
+            num_passenger += func4(passengers[i]);
+            num_passenger -= func3(passengers[i]);
+        }
+        int answer = func1(seat-num_passenger);
+        return answer;
+    }
+    public int func1(int num){
+        if(0 > num){
+            return 0;
+        }
+        else{
+            return num;
+        }
+    }
+    public int func2(int num){
+        if(num > 0){
+            return 0;
+        }
+        else{
+            return num;
+        }
+    }
+
+    public int func3(String[] station){
+        int num = 0;
+        for(int i=0; i<station.length; i++){
+            if(station[i].equals("Off")){
+                num += 1;
+            }
+        }
+        return num;
+    }
+
+    public int func4(String[] station){
+        int num = 0;
+        for(int i=0; i<station.length; i++){
+            if(station[i].equals("On")){
+                num += 1;
+            }
+        }
+        return num;
+    }*/
+    /* 3번 문제
+    public String solution(String nickname) {
+        String answer = "";
+        for(int i=0; i<nickname.length(); i++){
+            if(nickname.charAt(i) == 'l'){
+                answer += "I";
+            }
+            else if(nickname.charAt(i) == 'w'){
+                answer += "vv";
+            }
+            else if(nickname.charAt(i) == 'W'){
+                answer += "VV";
+            }
+            else if(nickname.charAt(i) == 'O'){
+                answer += "0";
+            }
+            else{
+                answer += nickname.charAt(i);
+            }
+        }
+        while(answer.length() <= 3){
+            answer += "o";
+        }
+        if(answer.length() > 8){
+            answer = answer.substring(0, 8);
+        }
+        return answer;
+    }*/
+    /* 4번 문제
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = (int)(Math.pow(c,2) - Math.pow(a,2));
+
+        System.out.println(b_square);
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
+
+        if (age_type.equals("Korea")) {
+            answer = 2030 - year + 1;
+        }
+        else if (age_type.equals("Year")) {
+            answer = 2030 - year;
+        }
+
+        System.out.println(answer);
+    }
+}
